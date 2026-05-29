7d6930498e72fd94bf785cf5fe71c67b73f11fc1
Nesanesis <orionvega2343@gmail.com>
2026-05-29 16:16:09 +0300

    feat: add connect db

--- Internal/db/db.go
+++ Internal/db/db.go
@@ -1,5 +1,18 @@
 package db
 
 import (
+	"TaskAPI/Internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
+
+func Connect(c config.Config) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf("port=%d host=%s user=%s password=%s dbname=%s sslmode=%s", c.DB.Port, c.DB.Host, c.DB.User, c.Password, c.DB.Name, c.DB.SSLMod)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
