a9ff17600141ffc80c60568550bfa3cb8df96185
Nesanesis <orionvega2343@gmail.com>
2026-05-31 01:37:33 +0300

    feat: add task model

--- Internal/models/task.go
+++ Internal/models/task.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Task struct {
+	Id        int       `json:"id" db:"id"`
+	Title     string    `json:"title" db:"title"`
+	Desc      string    `json:"desc" db:"desc"`
+	Status    string    `json:"status" db:"status"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
